a206e83057898b0c0c1ce0ed9b37316f1b2f8199
wisdom Ekpot <wisdomekpot@gmail.com>
2020-10-10 23:25:58 +0100

    redis crud

--- redis/redis.ts
+++ redis/redis.ts
@@ -7,6 +7,37 @@ const redis = await connect({
 console.log(await redis.ping())
 
 
-await redis.set('name', 'Wisdom Ekpot');
-let name = await redis.get('name')
-console.log(name)
+// await redis.set('name', 'Wisdom Ekpot');
+// let name = await redis.get('name')
+// console.log(name)
+
+
+// let names = await redis.keys('*');
+// console.log(names)
+
+
+let add = async(key:string,name:string,email:string) => {
+    let addPerson = await redis.hmset(key, {
+        'name': name,
+        'email': email
+    })
+
+    return addPerson
+}
+
+// console.log(await add('key1','Wisdom Ekpot','wisdomekpot@gmail.com'))
+
+
+// let getParticular = async (id:string) => {
+//    return await redis.hgetall(id);
+
+// }
+// console.log(await getParticular('key1'))
+
+
+let deleteKey = async (id:string) => {
+    let deleted = await redis.del(id);
+    return deleted
+}
+console.log(await deleteKey('key1'))
+
